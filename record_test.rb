08d21ade1d82252528fc4e8b543c7ddf921c1d1b
richdownie <richdownie@gmail.com>
2023-12-29 15:35:50 -0500

    adding record test

---
+++ record_test.rb
@@ -0,0 +1,30 @@
+require 'appium_lib'
+
+# Set your desired capabilities
+caps = {
+  "platformName": "iOS",
+  "appium:platformVersion": "17.2",
+  "appium:deviceName": "iPhone 15 Pro Max",
+  "appium:automationName": "XCUITest",
+  "appium:app": "com.apple.Preferences"
+}
+# Instantiate a new Appium Driver
+driver = Appium::Driver.new({ caps: caps, appium_lib: { server_url: 'http://127.0.0.1:4723/' } })
+
+# Start the Appium Driver
+driver.start_driver
+
+driver.start_recording_screen video_quality: 'high'
+
+# Perform actions on the app
+driver.find_element(name: "General").click
+sleep 1
+driver.find_element(name: "About").click
+
+# Stop recording screen
+recording = driver.driver.stop_recording_screen
+
+# Save recording to a file
+File.open('video.mp4', 'wb') do |file|
+  file.write(Base64.decode64(recording))
+end
