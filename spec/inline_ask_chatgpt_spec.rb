fdce43b1c31a218285cee129b6148425e4fc1dc1
richdownie <richdownie@gmail.com>
2024-03-25 09:38:04 -0400

    rc updates

--- spec/inline_ask_chatgpt_spec.rb
+++ spec/inline_ask_chatgpt_spec.rb
@@ -13,14 +13,12 @@ RSpec.describe 'ChatGPT' do
 
   it 'ask for accessibility id locator' do
     # Perform actions in the app
-    begin
-      driver.find_element(id: 'General').click
-      driver.find_element(id: 'Keyboard').click
-      driver.find_element(id: '2').click
-      driver.find_element(id: "Add New Keyboard...").click
-      expect(driver.find_element(id: 'Add New Keyboard').displayed?).to be true
-    rescue
-      ChatGPTHelper.new.ask(driver.page_source)
-    end
+    driver.find_element(id: 'General').click
+    driver.find_element(id: 'Keyboard').click
+    driver.find_element(id: '2').click
+    driver.find_element(id: 'Add New Keyboard...').click
+    expect(driver.find_element(id: 'Add New Keyboard').displayed?).to be true
+  rescue StandardError
+    ChatGPTHelper.new.ask(driver.page_source)
   end
 end
